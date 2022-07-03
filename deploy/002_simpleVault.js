f457d24914596883231be7f6f31dc3995e050de0
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-07-03 11:48:51 +0530

    feat: transfer ownership after LAYs deployment

--- deploy/002_simpleVault.js
+++ deploy/002_simpleVault.js
@@ -2,12 +2,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const laysTokenAddr = (await deployments.get("LAYsToken")).address;
+  const laysToken = await deployments.get("LAYsToken");
+  const laysTokenAddr = laysToken.address;
 
   await deploy("SimpleVault", {
     from: deployer,
     args: [laysTokenAddr, "0x0715A7794a1dc8e42615F059dD6e406A6594651A"],
     log: true,
   });
+
+  // change ownership of LAYs token to SimpleVault
+  const simpleVaultAddr = (await deployments.get("SimpleVault")).address;
+
+  const laysTokenContract = await ethers.getContract("LAYsToken", deployer);
+  await laysTokenContract.transferOwnership(simpleVaultAddr);
 };
 module.exports.tags = ["SimpleVault"];
